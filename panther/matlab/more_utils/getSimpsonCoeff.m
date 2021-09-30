4e1bd2be44295ec37aeccd062c99b40619c1b4e5
Jesus Tordesillas <jtorde@mit.edu>
2021-09-29 20:23:37 -0400

    minor comment

--- panther/matlab/more_utils/getSimpsonCoeff.m
+++ panther/matlab/more_utils/getSimpsonCoeff.m
@@ -2,11 +2,11 @@ function result=getSimpsonCoeff(j,total)
 
     j_is_even = (rem(j, 2) == 0);
 
-    %See Composite Simpson's rule in https://en.wikipedia.org/wiki/Simpson%27s_rule
+    %See Composite Simpson's rule in https://en.wikipedia.org/wiki/Simpson%27s_rule#Composite_Simpson's_rule
 
     if(j==1 || j==total)  %Beginning or end
         result=1.0;
-    elseif (j_is_even)
+    elseif (j_is_even) %Note that Matlab is 1-indexing (that's why this changes wrt to the above link)
         result=4.0;
     else 
         result=2.0;
