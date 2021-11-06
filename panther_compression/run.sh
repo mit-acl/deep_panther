7b702a31847fb331d3a68a5f5db83f8cb7b66538
Jesus Tordesillas <jtorde@mit.edu>
2021-11-06 10:43:37 -0400

    changed minor params

--- panther_compression/run.sh
+++ panther_compression/run.sh
@@ -2,9 +2,9 @@
     python3 policy_compression_train.py \
     --log_dir "evals/log_dagger" \
     --policy_dir "evals/tmp_dagger" \
-    --eval-ep-len 4 \
+    --eval-ep-len 200 \
     --dagger-beta 35 \
-    --n_iter 5 \
+    --n_iter 35 \
     --n_eval 5 \
     --seed 1
 
