2a4837ea171701ff9323ed55bbc17f0ee9d2fb15
Jesus Tordesillas <jtorde@mit.edu>
2021-11-17 13:45:33 -0500

    example of how to call the student

---
+++ panther_compression/call_student.py
@@ -0,0 +1,12 @@
+from imitation.algorithms import bc
+from compression.utils.other import ObservationManager
+
+if __name__ == "__main__":
+
+    student_policy = bc.reconstruct_policy("/home/jtorde/Desktop/ws/src/panther_plus_plus/panther_compression/evals/tmp_dagger/1/final_policy.pt")
+    om=ObservationManager();
+
+    obs=om.getRandomNormalizedObservation();
+    action = student_policy.predict(obs, deterministic=True)
+
+    print (action)
