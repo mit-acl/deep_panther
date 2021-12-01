10b9651da4ec017346d7f3764a7c63bb6a21f229
Jesus Tordesillas <jtorde@mit.edu>
2021-11-30 19:55:10 -0500

    minor

--- panther_compression/call_student.py
+++ panther_compression/call_student.py
@@ -3,7 +3,7 @@ from compression.utils.other import ObservationManager
 
 if __name__ == "__main__":
 
-    student_policy = bc.reconstruct_policy("/home/jtorde/Desktop/ws/src/panther_plus_plus/panther_compression/evals/tmp_dagger/1/final_policy.pt")
+    student_policy = bc.reconstruct_policy("/home/jtorde/Desktop/ws/src/panther_plus_plus/panther_compression/evals/tmp_dagger/2/final_policy.pt")
     om=ObservationManager();
 
     obs=om.getRandomNormalizedObservation();
