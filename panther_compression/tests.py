27f89e48c5d7c8314567ca1f9a6fbb02806076c1
Jesus Tordesillas <jtorde@mit.edu>
2021-12-08 12:31:22 -0500

    more tests

--- panther_compression/tests.py
+++ panther_compression/tests.py
@@ -70,10 +70,8 @@ print("================================")
 p0=np.array([[0],[0],[0]]);
 v0=np.array([[0],[0],[0]]);
 a0=np.array([[0],[0],[0]]);
-
 y0=np.array([[0]]);
 y_dot0=np.array([[0]]);
-
 my_state=State(p0, v0, a0, y0, y_dot0)
 
 f_action=am.getRandomAction()
@@ -88,6 +86,47 @@ f_SolOrGuess.printInfo()
 
 f_action_result=am.solOrGuess2action(f_SolOrGuess)
 
-assert np.linalg.norm(f_action_result-f_action)<1e7
+assert np.linalg.norm(f_action_result-f_action)<1e7, "Assertion failed"
 
 print("should be zeros= ",f_action_result-f_action)
+
+
+
+print("================================")
+
+p0=np.array([[0],[0],[0]]);
+v0=om.randomVel();
+a0=om.randomAccel();
+y0=om.randomYaw();
+y_dot0=om.randomYdot();
+my_wState=State(p0, v0, a0, y0, y_dot0)
+
+f_action=am.getRandomAction();
+
+w_posBS, w_yawBS=am.f_actionAnd_w_State2wBS(f_action, my_wState)
+
+print(f"w_yawBS.getPosT(0.0)={w_yawBS.getPosT(0.0)}")
+print(f"y0={y0}")
+# assert w_yawBS.getPosT(0.0)==y0, "Assertion failed"
+print("Test 1")
+np.testing.assert_allclose(w_yawBS.getPosT(0.0)-y0, 0, atol=1e-05)
+
+
+f_yaw_ctrl_pts=am.getYawCtrlPts(f_action);
+
+print("w_Last pos=",w_yawBS.getLastPos())
+print("f_yaw_ctrl_pts=",f_yaw_ctrl_pts)
+np.testing.assert_allclose(w_yawBS.getLastPos()-(f_yaw_ctrl_pts[0,-1]+y0), 0, atol=1e-07)
+
+
+print(f_yaw_ctrl_pts)
+print(f_yaw_ctrl_pts+y0*np.ones(f_yaw_ctrl_pts.shape))
+print(w_yawBS.ctrl_pts[0,2:-1].reshape(1,-1))
+should_be_zeros= w_yawBS.ctrl_pts[0,2:-1].reshape(1,-1)-(f_yaw_ctrl_pts+y0*np.ones(f_yaw_ctrl_pts.shape))
+print("Test 2")
+print("should_be_zeros= ",should_be_zeros)
+np.testing.assert_allclose(should_be_zeros, 0, atol=1e-07)
+
+
+
+print(w_yawBS.ctrl_pts)
