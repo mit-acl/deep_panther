9ee8da73ab291a7a33262fe4d34611dcf0e76dc3
Jesus Tordesillas <jtorde@mit.edu>
2022-01-12 13:13:57 -0500

    minor: changed loc obstacle

--- panther/scripts/dynamic_corridor.py
+++ panther/scripts/dynamic_corridor.py
@@ -223,7 +223,7 @@ class DynCorridor:
             # y_string='0'
             # z_string='1'
             x_string=str(random.uniform(2.5, 2.5))
-            y_string=str(random.uniform(-1.0, 1.0))
+            y_string=str(random.uniform(0.0, 0.0))
             z_string=str(random.uniform(1.0, 1.0))         
             self.all_dyn_traj[i].s_mean=[x_string, y_string, z_string]
             # #End of hack
