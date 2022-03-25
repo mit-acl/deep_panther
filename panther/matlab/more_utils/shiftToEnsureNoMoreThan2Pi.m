da5aa2554381c9e0fa4425008a425a0bfcdf94fc
Jesus Tordesillas <jtorde@mit.edu>
2022-03-25 12:49:30 -0400

    generated casadi function to use closed-form solution for yaw

--- panther/matlab/more_utils/shiftToEnsureNoMoreThan2Pi.m
+++ panther/matlab/more_utils/shiftToEnsureNoMoreThan2Pi.m
@@ -13,10 +13,18 @@ function all_phi_correct=shiftToEnsureNoMoreThan2Pi(all_phi)
         phi_i_f=phi_i+floor(differ/(2*pi))*2*pi;
         phi_i_c=phi_i+ceil(differ/(2*pi))*2*pi; 
 
-        if(abs((previous_phi-phi_i_f))<abs((previous_phi-phi_i_c)))
-            phi_i_corrected=phi_i_f;
-        else
-            phi_i_corrected=phi_i_c;
+        cond=abs((previous_phi-phi_i_f))<abs((previous_phi-phi_i_c));
+
+        if(isnumeric(phi_i_f))
+
+            if(cond)
+                phi_i_corrected=phi_i_f;
+            else
+                phi_i_corrected=phi_i_c;
+            end
+
+        else %For Casadi
+                phi_i_corrected = if_else(cond,phi_i_f,phi_i_c,false); %If short-circuit is true, it takes much longer
         end
 
 %         fprintf("previous_phi = %f\n", rad2deg(previous_phi))
