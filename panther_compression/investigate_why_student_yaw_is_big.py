8305f350b18263c3d1655c4468d7e0a18a734576
kotakondo <kkondo@mit.edu>
2023-03-02 12:10:54 -0500

    fix some bugs

--- panther_compression/investigate_why_student_yaw_is_big.py
+++ panther_compression/investigate_why_student_yaw_is_big.py
@@ -39,7 +39,6 @@ if __name__ == "__main__":
 	## params
 	##
 
-	yaw_scaling = getPANTHERparamsAsCppStruct().yaw_scaling
 	use_demo_obs_acts = False
 	calculate_loss = False
 	ENV_NAME = 	"my-environment-v1" # you shouldn't change the name
@@ -53,7 +52,7 @@ if __name__ == "__main__":
 	rospack = rospkg.RosPack()
 	path_panther=rospack.get_path('panther')
 	if use_demo_obs_acts:
-		data = load(path_panther[:-8]+'/panther_compression/evals/tmp_dagger/2/demos/round-004/dagger-demo-20230223_212805_2379ee.npz')
+		data = load(path_panther[:-8]+'/panther_compression/evals/tmp_dagger/2/demos/round-000/dagger-demo-20230301_194313_156b74.npz')
 		lst = data.files
 
 	##
@@ -68,8 +67,18 @@ if __name__ == "__main__":
 	##
 
 	expert_policy = ExpertPolicy()
-	path_file=path_panther[:-8]+"/panther_compression/trained_policies/Hung_dynamic_obstacles.pt"
-	# path_file=path_panther[:-8]+"/panther_compression/evals/tmp_dagger/2/64_intermediate_policy_round23_log9.pt"
+	# path_file=path_panther[:-8]+"/panther_compression/trained_policies/Hung_dynamic_obstacles.pt"
+	path_file=path_panther[:-8]+"/panther_compression/trained_policies/pos_yaw_2000_intermediate_policy_round5_log18.pt"
+	# path_file=path_panther[:-8]+"/panther_compression/evals/tmp_dagger/2/intermediate_policy_round14_log28.pt"
+	
+	# make sure you chage yaw_scaling factor if I am using a different value
+	# yaw_scaling = getPANTHERparamsAsCppStruct().yaw_scaling
+	yaw_scaling = 2000.0
+	# yaw_scaling = 1.0
+
+
+
+	# path_file=path_panther[:-8]+"/panther_compression/evals/tmp_dagger/2/intermediate_policy_round5_log99.pt"
 	student_policy = bc.reconstruct_policy(path_file) #got this from test_infeasibility_ipopt.py
 
 	##
@@ -78,13 +87,13 @@ if __name__ == "__main__":
 	
 	## note that demo's obs and acts are tensor and visualization is not supported yet in this file (you can see loss tho)
 	if use_demo_obs_acts:
-		obs = th.as_tensor(data["obs"]).detach()
+		obs = th.as_tensor(data["obs"])
 		acts_expert = th.as_tensor(data["acts"]).detach()
 	else:
-		obstacle_position = np.array([[5.5],[0.0],[1.0]])
-		goal_position = np.array([[10.0],[0.0],[1.0]])
+		obstacle_position = np.array([[5.0],[0.0],[1.0]])
+		goal_position = np.array([[10.0],[0.0],[0.0]])
 		venv.changeConstantObstacleAndGtermPos(obstacle_position, goal_position)
-		obs = venv.reset()
+		obs_n = venv.reset()
 
 	##
 	## initial condition
@@ -105,7 +114,7 @@ if __name__ == "__main__":
 	## traj_expert is just a reshaped control points (the last element is total time)
 	## w_posBS_expert is a B-spline
 
-	acts_n_expert_np = expert_policy.predict(obs, deterministic=False)
+	acts_n_expert_np = expert_policy.predict(obs_n, deterministic=True)
 	acts_n_expert_np = acts_n_expert_np[0].reshape(venv.am.getActionShape())
 	venv.am.assertAction(acts_n_expert_np)
 	venv.am.assertActionIsNormalized(acts_n_expert_np)
@@ -118,20 +127,32 @@ if __name__ == "__main__":
 	## get BS for student
 	##
 
-	acts_n = student_policy.predict(obs, deterministic=False)
+	acts_n = student_policy.predict(obs_n, deterministic=True)
 	# acts_student = student_policy.forward(obs[0], deterministic=True)
 	# acts_student = acts_student.detach().numpy()[0]
 	# scale back down the acts_student
 	# acts_n = student_policy.forward(obs[0], deterministic=True)
-	acts_n_student=(acts_n[0].reshape(venv.am.getActionShape())) 
-	venv.am.assertAction(acts_n_student)
-	venv.am.assertActionIsNormalized(acts_n_student)
-	acts_student = venv.am.denormalizeAction(acts_n_student)
 
-	# scale back down the acts_student
-	acts_student[:,venv.am.traj_size_pos_ctrl_pts:venv.am.traj_size_pos_ctrl_pts+venv.am.traj_size_yaw_ctrl_pts] = acts_student[:,venv.am.traj_size_pos_ctrl_pts:venv.am.traj_size_pos_ctrl_pts+venv.am.traj_size_yaw_ctrl_pts]/yaw_scaling
-	traj_student = venv.am.getTrajFromAction(acts_student, 0)
-	w_posBS_student, w_yawBS_student= venv.am.f_trajAnd_w_State2wBS(traj_student, State(p0, v0, a0, y0, ydot0))
+
+	w_posBS_student = []
+	w_yawBS_student = []
+	print("len(acts_n[0]): ", len(acts_n[0]))
+	for i in range(len(acts_n[0])):
+
+		acts_n_student=(acts_n[0].reshape(venv.am.getActionShape())) 
+		venv.am.assertAction(acts_n_student)
+		venv.am.assertActionIsNormalized(acts_n_student)
+		acts_student = venv.am.denormalizeAction(acts_n_student)
+
+		# scale back down the acts_student
+		acts_student[:,venv.am.traj_size_pos_ctrl_pts:venv.am.traj_size_pos_ctrl_pts+venv.am.traj_size_yaw_ctrl_pts] = acts_student[:,venv.am.traj_size_pos_ctrl_pts:venv.am.traj_size_pos_ctrl_pts+venv.am.traj_size_yaw_ctrl_pts]/yaw_scaling
+		print("len(acts_student): ",len(acts_student)) 
+		traj_student = venv.am.getTrajFromAction(acts_student, i)
+		w_posBS_student_tmp, w_yawBS_student_tmp = venv.am.f_trajAnd_w_State2wBS(traj_student, State(p0, v0, a0, y0, ydot0))
+
+		w_posBS_student.append(w_posBS_student_tmp)
+		w_yawBS_student.append(w_yawBS_student_tmp)
+
 
 	##
 	## get ydot_max
@@ -146,7 +167,7 @@ if __name__ == "__main__":
 	## calculate loss (ref:: bc.py)
 	##
 
-	## note that this is only for tensor obs acts (in case use_demos_obs_acts == True)
+	# note that this is only for tensor obs acts (in case use_demos_obs_acts == True)
 	if (calculate_loss):
 		num_of_traj_per_action=list(acts_expert_np.shape)[1] #acts_expert_np.shape is [batch size, num_traj_action, size_traj]
 		num_of_elements_per_traj=list(acts_expert_np.shape)[2] #acts_expert_np.shape is [batch size, num_traj_action, size_traj]
@@ -240,15 +261,20 @@ if __name__ == "__main__":
 
 	fig = plt.figure()
 	ax = fig.add_subplot(projection='3d')
-	xx = np.linspace(w_posBS_expert.getT0(), w_posBS_expert.getTf(), 100)
+	xx_exp = np.linspace(w_posBS_expert.getT0(), w_posBS_expert.getTf(), 100)
+	xx_stu = []
+	for i in range(len(acts_n[0])):
+		xx_stu.append(np.linspace(w_yawBS_student[i].getT0(), w_yawBS_student[i].getTf(), 100))
 	ax.set_title('3d pos')
 
 	##
 	## trajs
 	##
 
-	ax.plot(w_posBS_expert.pos_bs[0](xx), w_posBS_expert.pos_bs[1](xx), w_posBS_expert.pos_bs[2](xx), lw=4, alpha=0.7, label='expert traj')
-	ax.plot(w_posBS_student.pos_bs[0](xx), w_posBS_student.pos_bs[1](xx), w_posBS_student.pos_bs[2](xx), lw=4, alpha=0.7, label='student traj')
+	ax.plot(w_posBS_expert.pos_bs[0](xx_exp), w_posBS_expert.pos_bs[1](xx_exp), w_posBS_expert.pos_bs[2](xx_exp), lw=4, alpha=0.7, label='expert traj')
+	
+	for i in range(len(acts_n[0])):
+		ax.plot(w_posBS_student[i].pos_bs[0](xx_stu[i]), w_posBS_student[i].pos_bs[1](xx_stu[i]), w_posBS_student[i].pos_bs[2](xx_stu[i]), lw=4, alpha=0.7, label=f'student traj #{i}')
 	
 	##
 	## box
@@ -264,16 +290,33 @@ if __name__ == "__main__":
 	y += obstacle_position[1]
 	z += obstacle_position[2]
 	ax.plot_surface(x, y, z, alpha=0.2, color='brown')
+
+	ax.grid(True)
+	ax.legend(loc='best')
+	ax.set_xlim(-2, 7)
+	ax.set_ylim(-2, 7)
+	ax.set_zlim(-2, 7)
+	ax.set_xlabel('X')
+	ax.set_ylabel('Y')
+	ax.set_zlabel('Z')
+	ax.set_aspect('equal')
+	fig.savefig('/home/kota/Research/deep-panther_ws/src/deep_panther/panther/matlab/figures/test_pos.png')
+	# plt.show()
 	
 	##
 	## yaw vector
 	##
 
 	num_vectors = 10
-	xx2 = np.linspace(w_posBS_expert.getT0(), w_posBS_expert.getTf(), num_vectors)
-	initialized = False # for quiver label
-	for xx2_i in xx2:
-		# for expert
+	xx2_exp = np.linspace(w_posBS_expert.getT0(), w_posBS_expert.getTf(), num_vectors)
+	xx2_stu = []
+	for i in range(len(acts_n[0])):
+		xx2_stu.append(np.linspace(w_yawBS_student[i].getT0(), w_yawBS_student[i].getTf(), num_vectors))
+	initialized_exp = False # for quiver label
+	initialized_stu = False # for quiver label
+	# for expert
+
+	for xx2_i in xx2_exp:
 		xs_expert = w_posBS_expert.pos_bs[0](xx2_i)
 		ys_expert = w_posBS_expert.pos_bs[1](xx2_i)
 		zs_expert = w_posBS_expert.pos_bs[2](xx2_i)
@@ -281,21 +324,28 @@ if __name__ == "__main__":
 		xe_expert = 1.0 * np.cos(yaw_expert)
 		ye_expert = 1.0 * np.sin(yaw_expert)
 		ze_expert = 0
-		# for student
-		xs_student = w_posBS_student.pos_bs[0](xx2_i)
-		ys_student = w_posBS_student.pos_bs[1](xx2_i)
-		zs_student = w_posBS_student.pos_bs[2](xx2_i)
-		yaw_student = w_yawBS_student.pos_bs[0](xx2_i)
-		xe_student = 1.0 * np.cos(yaw_student)
-		ye_student = 1.0 * np.sin(yaw_student)
-		ze_student = 0
-		if not initialized:
+		if not initialized_exp:
 			ax.quiver(xs_expert, ys_expert, zs_expert, xe_expert, ye_expert, ze_expert, color='red', label='expert')
-			ax.quiver(xs_student, ys_student, zs_student, xe_student, ye_student, ze_student, color='blue', label='student')
-			initialized = True
+			initialized_exp = True
 		else:
 			ax.quiver(xs_expert, ys_expert, zs_expert, xe_expert, ye_expert, ze_expert, color='red')
-			ax.quiver(xs_student, ys_student, zs_student, xe_student, ye_student, ze_student, color='blue')
+
+	# for student
+	for i in range(len(acts_n[0])):
+		for xx2_i in xx2_stu[i]:
+			xs_student = w_posBS_student[i].pos_bs[0](xx2_i)
+			ys_student = w_posBS_student[i].pos_bs[1](xx2_i)
+			zs_student = w_posBS_student[i].pos_bs[2](xx2_i)
+			yaw_student = w_yawBS_student[i].pos_bs[0](xx2_i)
+			xe_student = 1.0 * np.cos(yaw_student)
+			ye_student = 1.0 * np.sin(yaw_student)
+			ze_student = 0
+			if not initialized_stu:
+				ax.quiver(xs_student, ys_student, zs_student, xe_student, ye_student, ze_student, color='blue', label='student')
+				initialized_stu = True
+			else:
+				ax.quiver(xs_student, ys_student, zs_student, xe_student, ye_student, ze_student, color='blue')
+		
 	ax.grid(True)
 	ax.legend(loc='best')
 	ax.set_xlim(-2, 7)
@@ -314,33 +364,40 @@ if __name__ == "__main__":
 	
 	fig, ax = plt.subplots(2,1)
 	fig.tight_layout(pad=2.0)
-	xx = np.linspace(w_yawBS_expert.getT0(), w_yawBS_expert.getTf(), 100)
+
 	ax[0].set_title('Yaw comparison')
-	ax[0].plot(xx, w_yawBS_expert.pos_bs[0](xx), 'r-', lw=4, alpha=0.7, label='expert yaw')
-	ax[0].plot(xx, w_yawBS_student.pos_bs[0](xx), 'b-', lw=4, alpha=0.7, label='student yaw')
+	ax[0].plot(xx_exp, w_yawBS_expert.pos_bs[0](xx_exp), 'r-', lw=4, alpha=0.7, label='expert yaw')
+	for i in range(len(acts_n[0])):
+		ax[0].plot(xx_stu[i], w_yawBS_student[i].pos_bs[0](xx_stu[i]), 'b-', lw=4, alpha=0.7, label=f'student yaw #{i}')
 	ax[0].grid(True)
 	ax[0].legend(loc='best')
 
 	ax[1].set_title('Derivative of Yaw comparison')
-	ax[1].plot(xx, w_yawBS_expert.vel_bs[0](xx), 'r-', lw=4, alpha=0.7, label='expert dyaw')
-	ax[1].plot(xx, w_yawBS_student.vel_bs[0](xx), 'b-', lw=4, alpha=0.7, label='student dyaw')
+	ax[1].plot(xx_exp, w_yawBS_expert.vel_bs[0](xx_exp), 'r-', lw=4, alpha=0.7, label='expert dyaw')
+	for i in range(len(acts_n[0])):
+		ax[1].plot(xx_stu[i], w_yawBS_student[i].vel_bs[0](xx_stu[i]), lw=4, alpha=0.7, label=f'student dyaw #{i}')
 	ax[1].grid(True)
 	ax[1].legend(loc='best')
 	fig.savefig('/home/kota/Research/deep-panther_ws/src/deep_panther/panther/matlab/figures/test_yaw.png')
 	plt.show()
+
+	sys.exit(0)
 
 	##
 	## check constraint violation
 	##
 
 	exp_violated = False
-	stu_violated = False
-	for xx_i in xx:
+	for xx_i in xx_exp:
 		if abs(w_yawBS_expert.vel_bs[0](xx_i)) > yaw_dot_max and not exp_violated:
 			print('expert violates ydot constraint')
 			exp_violated = True
-		if abs(w_yawBS_student.vel_bs[0](xx_i)) > yaw_dot_max and not stu_violated:
-			print('student violates ydot constraint') 
-			stu_violated = True
+
+	for i in range(len(acts_n[0])):
+		stu_violated = False
+		for xx_i in xx_stu[i]:
+			if abs(w_yawBS_student.vel_bs[0](xx_i)) > yaw_dot_max and not stu_violated:
+				print(f'student #{i} violates ydot constraint') 
+				stu_violated = True
 
 	sys.exit(0)
