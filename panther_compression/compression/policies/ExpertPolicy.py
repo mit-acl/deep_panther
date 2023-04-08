fixed how to collect computation time

panther_compression/compression/policies/ExpertPolicy.py
@@ -165,4 +165,4 @@ class ExpertPolicy(object):
 
         acts = np.stack(acts, axis=0)
 
-        return acts, np.mean(computation_times)
+        return acts, computation_times

panther_compression/compression/policies/StudentPolicy.py
@@ -298,4 +298,4 @@ class StudentPolicy(BasePolicy):
             acts.append(self.predict( obs_n[i,:], deterministic=deterministic)[0].reshape(self.am.getActionShape()))
         # acts=[self.predict( obs_n[i, :], deterministic=deterministic)[0].reshape(self.am.getActionShape()) for i in range(len(obs_n))] #Note that len() returns the size along the first axis
         acts=np.stack(acts, axis=0)
-        return acts, mean(self.computation_times)
+        return acts, self.computation_times