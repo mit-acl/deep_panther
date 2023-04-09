2293889d0d05304e7df3a33849912653eadc85f9
kotakondo <kkondo@mit.edu>
2023-04-09 16:26:47 -0400

    run_many_sims.py added

---
+++ panther/scripts/goal_reached_checker.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#Author: Kota Kondo
+#Date: April 8 2023
+
+import math
+import os
+import sys
+import time
+import rospy
+import rosgraph
+from geometry_msgs.msg import PoseStamped
+from snapstack_msgs.msg import State
+from panther_msgs.msg import GoalReached
+import numpy as np
+from random import *
+import tf2_ros
+from numpy import linalg as LA
+from run_many_sims import get_start_end_state
+
+class GoalReachedCheck:
+
+    def __init__(self, num_of_agents, circle_radius):
+
+        rospy.sleep(3)
+
+        # goal radius
+        self.goal_radius = 0.30
+
+        # number of agents
+        self.num_of_agents = num_of_agents
+
+        # is initialized?
+        self.initialized = False
+
+        # starting time to calculate completion time
+        now = rospy.get_rostime()
+        self.starting_time = now.secs + now.nsecs / 1e9
+
+        # state and term_goal
+        self.state_pos = np.empty([self.num_of_agents,3])
+        self.term_goal_pos = np.empty([self.num_of_agents,3])
+
+        # publisher init
+        self.goal_reached = GoalReached()
+        self.pubIsGoalReached = rospy.Publisher('/sim_all_agents_goal_reached', GoalReached, queue_size=1, latch=True)
+
+        # is goal reached
+        self.is_goal_reached = False
+
+        # keep track of which drone has already got to the goal
+        self.is_goal_reached_cnt = 0
+        self.is_goal_reached_check_list = [False for i in range(self.num_of_agents)]
+
+    # goal reached checker
+    def goal_reached_checker(self, timer):
+        if not self.is_goal_reached and self.initialized:
+            for i in range(self.num_of_agents):
+                if self.is_goal_reached_check_list[i] == False:
+                    if (LA.norm(self.state_pos[i,:] - self.term_goal_pos[i,:]) > self.goal_radius):
+                        # print(i)
+                        # print(self.state_pos)
+                        # print(self.term_goal_pos[i,:])
+                        # print(LA.norm(self.state_pos[i,:] - self.term_goal_pos[i,:]))
+                        return
+                    else:
+                        self.is_goal_reached_check_list[i] = True
+                        self.is_goal_reached_cnt = self.is_goal_reached_cnt + 1
+                        # print(self.is_goal_reached_cnt)
+                        # print(i)
+                        # print(self.state_pos[i,:])
+                        # print(self.term_goal_pos[i,:])
+
+                        if self.is_goal_reached_cnt == self.num_of_agents:
+                            self.is_goal_reached = True
+                            now = rospy.get_rostime()
+                            self.goal_reached.header.stamp = now
+                            self.goal_reached.travel_time = now.secs + now.nsecs / 1e9 - self.starting_time
+                            self.goal_reached.is_goal_reached = True
+                            self.pubIsGoalReached.publish(self.goal_reached)
+
+    def SQ01stateCB(self, data):
+        self.state_pos[0,0:3] = np.array([data.pos.x, data.pos.y, data.pos.z])
+    def SQ02stateCB(self, data):
+        self.state_pos[1,0:3] = np.array([data.pos.x, data.pos.y, data.pos.z])
+    def SQ03stateCB(self, data):
+        self.state_pos[2,0:3] = np.array([data.pos.x, data.pos.y, data.pos.z])
+    def SQ04stateCB(self, data):
+        self.state_pos[3,0:3] = np.array([data.pos.x, data.pos.y, data.pos.z])
+    def SQ05stateCB(self, data):
+        self.state_pos[4,0:3] = np.array([data.pos.x, data.pos.y, data.pos.z])
+    def SQ06stateCB(self, data):
+        self.state_pos[5,0:3] = np.array([data.pos.x, data.pos.y, data.pos.z])
+    def SQ07stateCB(self, data):
+        self.state_pos[6,0:3] = np.array([data.pos.x, data.pos.y, data.pos.z])
+    def SQ08stateCB(self, data):
+        self.state_pos[7,0:3] = np.array([data.pos.x, data.pos.y, data.pos.z])
+    def SQ09stateCB(self, data):
+        self.state_pos[8,0:3] = np.array([data.pos.x, data.pos.y, data.pos.z])
+    def SQ10stateCB(self, data):
+        self.state_pos[9,0:3] = np.array([data.pos.x, data.pos.y, data.pos.z])
+    # def SQ11stateCB(self, data):
+    #     self.state_pos[10,0:3] = np.array([data.pos.x, data.pos.y, data.pos.z])
+    # def SQ12stateCB(self, data):
+    #     self.state_pos[11,0:3] = np.array([data.pos.x, data.pos.y, data.pos.z])
+    # def SQ13stateCB(self, data):
+    #     self.state_pos[12,0:3] = np.array([data.pos.x, data.pos.y, data.pos.z])
+    # def SQ14stateCB(self, data):
+    #     self.state_pos[13,0:3] = np.array([data.pos.x, data.pos.y, data.pos.z])
+    # def SQ15stateCB(self, data):
+    #     self.state_pos[14,0:3] = np.array([data.pos.x, data.pos.y, data.pos.z])
+    # def SQ16stateCB(self, data):
+    #     self.state_pos[15,0:3] = np.array([data.pos.x, data.pos.y, data.pos.z])
+
+
+    def SQ01term_goalCB(self, data):
+        self.term_goal_pos[0,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+        self.initialized = True
+    def SQ02term_goalCB(self, data):
+        self.term_goal_pos[1,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    def SQ03term_goalCB(self, data):
+        self.term_goal_pos[2,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    def SQ04term_goalCB(self, data):
+        self.term_goal_pos[3,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    def SQ05term_goalCB(self, data):
+        self.term_goal_pos[4,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    def SQ06term_goalCB(self, data):
+        self.term_goal_pos[5,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    def SQ07term_goalCB(self, data):
+        self.term_goal_pos[6,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    def SQ08term_goalCB(self, data):
+        self.term_goal_pos[7,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    def SQ09term_goalCB(self, data):
+        self.term_goal_pos[8,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    def SQ10term_goalCB(self, data):
+        self.term_goal_pos[9,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    # def SQ11term_goalCB(self, data):
+    #     self.term_goal_pos[10,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    # def SQ12term_goalCB(self, data):
+    #     self.term_goal_pos[11,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    # def SQ13term_goalCB(self, data):
+    #     self.term_goal_pos[12,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    # def SQ14term_goalCB(self, data):
+    #     self.term_goal_pos[13,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    # def SQ15term_goalCB(self, data):
+    #     self.term_goal_pos[14,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+    # def SQ16term_goalCB(self, data):
+    #     self.term_goal_pos[15,0:3] = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+
+def startNode(num_agents, circle_radius):
+
+    c = GoalReachedCheck(num_agents, circle_radius)
+
+    ## Subscribe to the state of each agent and the terminal goal
+    for i in range(num_agents):
+        call_back_function = getattr(c, "SQ%02dstateCB" % (i+1))
+        rospy.Subscriber("SQ%02ds/state" % (i+1), State, call_back_function)
+        call_back_function = getattr(c, "SQ%02dterm_goalCB" % (i+1))
+        rospy.Subscriber("SQ%02ds/term_goal" % (i+1), PoseStamped, call_back_function)
+    
+    rospy.Timer(rospy.Duration(1.0), c.goal_reached_checker)
+    rospy.spin()
+
+if __name__ == '__main__':
+
+    ##
+    ## get params
+    ##
+
+    num_of_agents = rospy.get_param("num_of_agents", 1)
+    circle_radius = rospy.get_param("circle_radius", 10.0)
+
+    rospy.init_node('goalReachedCheck')
+    startNode(num_of_agents, circle_radius)
--- panther/scripts/run_many_sims.py
+++ panther/scripts/run_many_sims.py
@@ -2,13 +2,12 @@
 # coding=utf-8
 
 # /* ----------------------------------------------------------------------------
-#  * Copyright 2020, Jesus Tordesillas Torres, Aerospace Controls Laboratory
+#  * Copyright 2023, Kota Kondo, Aerospace Controls Laboratory
 #  * Massachusetts Institute of Technology
 #  * All Rights Reserved
 #  * Authors: Jesus Tordesillas, et al.
 #  * See LICENSE file for the license information
 #  * -------------------------------------------------------------------------- */
-
 
 import math
 import os
@@ -18,78 +17,164 @@ import rospy
 from snapstack_msgs.msg import State
 import subprocess
 
+def get_start_end_state(num_of_agents, circle_radius) -> (list, list, list, list, list, list, list):
+    """
+    get start state for the agent_id-th agent
+    the agents will be placed in a circle with circle_radius radius, and the goal will be placed in the opposite direction
+    the first agent will be placed at x=circle_radius, y=0, z =0, yaw=pi, and the rest will be placed in a counther clockwise fashion 
+    """
+
+    ## Note: the exactly same function is defined in panther/scripts/goal_reached_checker.py
+    ## So if you change this function, you need to change the other one as well
+
+    x_start_list = []
+    y_start_list = []
+    z_start_list = []
+    yaw_start_list = []
+    x_end_list = []
+    y_end_list = []
+    z_end_list = []
+
+    for i in range(num_of_agents):
+        angle = 2*math.pi/num_of_agents * i
+        x_start_list.append(circle_radius*math.cos(angle))
+        y_start_list.append(circle_radius*math.sin(angle))
+        z_start_list.append(0.0)
+        yaw_start_list.append(math.tan(y_start_list[i]/x_start_list[i]) + math.pi)
+        x_end_list.append(-x_start_list[i])
+        y_end_list.append(-y_start_list[i])
+        z_end_list.append(0.0)
+
+    return x_start_list, y_start_list, z_start_list, yaw_start_list, x_end_list, y_end_list, z_end_list
+
+def check_goal_reached():
+    try:
+        is_goal_reached = subprocess.check_output(['rostopic', 'echo', '/sim_all_agents_goal_reached', '-n', '1'], timeout=2).decode()
+        print("True")
+        return True 
+    except:
+        print("False")
+        return False  
+
 if __name__ == '__main__':
 
+    ##
+    ## Parameters
+    ##
 
-    num_of_sims=30;
-    num_of_obs=[5]#[1000]#[50,400,500,600,700]#[150, 200, 250, 300, 350] #[340,380,420,460,500]; #140,180,220,260,300
-    commands = []
+    NUM_OF_SIMS = 100
+    NUM_OF_AGENTS = 2
+    NUM_OF_OBS_LIST = [5, 10, 15]
+    CIRCLE_RADIUS = 10.0
+    USE_PERFECT_CONTROLLER = "true"
+    USE_PERFECT_PREDICTION = "true"
+    SIM_DURATION = 60 # in seconds
+    HOME_DIR = "/media/kota/T7/deep-panther/bags" #TODO: hard-coded
+    RECORD_NODE_NAME = "bag_recorder"
+    KILL_ALL = "tmux kill-server & killall -9 gazebo & killall -9 gzserver  & killall -9 gzclient & killall -9  & killall -9 rosmaster & pkill panther_node & pkill -f dynamic_obstacles & pkill -f rosout & pkill -f behavior_selector_node & pkill -f rviz & pkill -f rqt_gui & pkill -f perfect_tracker & pkill -f panther_commands"
 
-    folder_bags="/home/jtorde/Desktop/ws/src/panther/panther/bags";
-    all_modes=["zhejiang", "panther", "noPA", "py", "ysweep"] #   "zhejiang", "panther", "noPA", "py", "ysweep"
-    name_node_record="bag_recorder"
-    kill_all="tmux kill-server & killall -9 gazebo & killall -9 gzserver  & killall -9 gzclient & killall -9 roscore & killall -9 rosmaster & pkill panther_node & pkill -f dynamic_obstacles & pkill -f rosout & pkill -f behavior_selector_node & pkill -f rviz & pkill -f rqt_gui & pkill -f perfect_tracker & pkill -f panther_commands"
+    ##
+    ## make sure ROS (and related stuff) is not running
+    ##
 
-    #make sure ROS (and related stuff) is not running
-    os.system(kill_all)
+    os.system(KILL_ALL)
 
+    ##
+    ## simulation loop
+    ##
 
-    for k in range(len(num_of_obs)):
-        for j in range(len(all_modes)):
-            mode=all_modes[j]
+    for k in range(len(NUM_OF_OBS_LIST)):
 
-            for s in range(num_of_sims):
+        ##
+        ## set up folders
+        ##
 
-                commands = []
+        folder_bags = HOME_DIR + f"/{NUM_OF_OBS_LIST[k]}_obs"
+        if not os.path.exists(folder_bags):
+            os.makedirs(folder_bags)
+        
+        for s in range(NUM_OF_SIMS):
 
-                # commands.append("roslaunch panther panther_specific_and_general.launch gui:=false rviz:=false environment:=false");
-                # commands.append("sleep 1.0 &&rosrun panther dynamic_corridor.py "+str(num_of_obs[k]));
-                # commands.append("sleep 3.0 && rosparam set /SQ01s/panther/mode "+mode); #Remember to comment the parameter "mode" in panther.yaml before running this file
-                # commands.append("sleep 3.0 && rosparam set /SQ01s/panther/visual false"); #Remember to comment the parameter "visual" in panther.yaml before running this file
+            ##
+            ## commands list initialized
+            ##
 
-                time_sleep=max(0.2*num_of_obs[k], 2.0)
+            commands = []
 
-                if(mode=="zhejiang"):
-                   commands.append("roslaunch ego_planner swarm.launch rviz:=false num_of_obs:="+str(num_of_obs[k]));
-                else:
-                   commands.append("roslaunch panther simulation.launch gazebo:=true perfect_tracker:=true perfect_prediction:=true quad:=SQ01s gui_mission:=false rviz:=false mode:="+mode+" num_of_obs:="+str(num_of_obs[k]));
+            time_sleep = max(0.2*NUM_OF_OBS_LIST[k], 2.0)
+            time_sleep_goal = min(NUM_OF_OBS_LIST[k], NUM_OF_AGENTS, 10.0)
 
-                commands.append("sleep "+str(time_sleep)+" && cd "+folder_bags+" && rosbag record -o "+mode+"_obs_"+str(num_of_obs[k])+"_sim_"+str(s)+" /SQ01s/goal /SQ01s/state /tf /tf_static /SQ01s/panther/fov /obstacles_mesh __name:="+name_node_record);
-                #publishing the goal should be the last command
-                commands.append("sleep "+str(time_sleep)+" && rostopic pub /SQ01s/term_goal geometry_msgs/PoseStamped \'{header: {stamp: now, frame_id: \"world\"}, pose: {position: {x: 40, y: 0, z: 1}, orientation: {w: 1.0}}}\'");
+            ###
+            ### simulation set up
+            ###
 
-                print("len(commands)= " , len(commands))
-                session_name="run_many_sims_single_agent_session"
-                os.system("tmux kill-session -t" + session_name)
+            ## roscore
+            commands.append("roscore")
 
-                os.system("tmux new -d -s "+str(session_name)+" -x 300 -y 300")
+            ## sim_basestation
+            commands.append(f"roslaunch --wait panther sim_base_station.launch num_of_obs:={NUM_OF_OBS_LIST[k]} rviz:=true gui_mission:=false")
 
-                for i in range(len(commands)):
-                    print('splitting ',i)
-                    os.system('tmux split-window ; tmux select-layout tiled')
-               
-                for i in range(len(commands)):
-                    os.system('tmux send-keys -t '+str(session_name)+':0.'+str(i) +' "'+ commands[i]+'" '+' C-m')
+            ## sim_onboard
+            x_start_list, y_start_list, z_start_list, yaw_start_list, x_end_list, y_end_list, z_end_list = get_start_end_state(NUM_OF_AGENTS, CIRCLE_RADIUS)
+            for i, (x, y, z, yaw) in enumerate(zip(x_start_list, y_start_list, z_start_list, yaw_start_list)):
+                agent_name = f"SQ{str(i+1).zfill(2)}s"
+                commands.append(f"roslaunch --wait panther sim_onboard.launch quad:={agent_name} perfect_controller:={USE_PERFECT_CONTROLLER} perfect_prediction:={USE_PERFECT_PREDICTION} x:={x} y:={y} z:={z} yaw:={yaw}")
+            
+            ## rosbag record
+            # commands.append("sleep "+str(time_sleep)+" && cd "+folder_bags+" && rosbag record -a -o sim_"+str(s)+" __name:="+RECORD_NODE_NAME)
+            
+            ## goal checker
+            commands.append(f"sleep {time_sleep} && roslaunch --wait panther goal_reached_checker.launch num_of_agents:={NUM_OF_AGENTS} circle_radius:={CIRCLE_RADIUS}")
 
-                print("Commands sent")
+            ## publish goal
+            for i, (x, y, z) in enumerate(zip(x_end_list, y_end_list, z_end_list)):
+                agent_name = f"SQ{str(i+1).zfill(2)}s"
+                ## TODO: may need to change the goal orientation
+                commands.append(f"sleep "+str(time_sleep_goal)+" && rostopic pub /"+agent_name+"/term_goal geometry_msgs/PoseStamped \'{header: {stamp: now, frame_id: \"world\"}, \
+                    pose: {position: {x: "+str(x)+", y: "+str(y)+", z: "+str(z)+"}, orientation: {w: 1.0}}}\'")
 
-                time.sleep(3.0)
-                pos_string=""
-                while (pos_string.find('39.')==-1 and pos_string.find('40.')==-1): #note the point after the 49, to make sure they are the first two numbers, and not the decimals
-                    try:
-                        pos_string =str(subprocess.check_output(['rostopic', 'echo', '/SQ01s/state/pos/x', '-n', '1']))
-                        print("Currently at ",pos_string, "[Sim "+str(s)+", with mode="+mode+", with num_of_obs="+str(num_of_obs[k])+"]");
-                    except:
-                        print("An Error occurred")
+            ##
+            ## tmux & sending commands
+            ##
 
+            session_name="run_many_sims_multiagent_session"
+            os.system("tmux kill-session -t" + session_name)
+            os.system("tmux new -d -s "+str(session_name)+" -x 300 -y 300")
 
-                print("Currently at ",pos_string)
+            for i in range(len(commands)):
+                os.system('tmux split-window ; tmux select-layout tiled')
+            
+            for i in range(len(commands)):
+                os.system('tmux send-keys -t '+str(session_name)+':0.'+str(i) +' "'+ commands[i]+'" '+' C-m')
+
+            print("commands sent")
+            time.sleep(3.0)
+
+            ##
+            ## wait until the goal is reached
+            ##
+
+            is_goal_reached = False
+            tic = time.perf_counter()
+            toc = time.perf_counter()
+
+            while (toc - tic < SIM_DURATION and not is_goal_reached):
+                toc = time.perf_counter()
+                if(check_goal_reached()):
+                    print('all the agents reached the goal')
+                    is_goal_reached = True
+                time.sleep(0.1)
+
+            if (not is_goal_reached):
+                os.system(f'echo "simulation {s}: not goal reached" >> '+folder_bags+'/status.txt')
+                print("Goal is not reached, killing the bag node")
+            else:
+                os.system(f'echo "simulation {s}: goal reached" >> '+folder_bags+'/status.txt')
                 print("Goal is reached, killing the bag node")
-                os.system("rosnode kill "+name_node_record);
-                time.sleep(0.5)
-                print("Killing the rest")
-                os.system(kill_all)
+
+            os.system("rosnode kill "+RECORD_NODE_NAME)
+            time.sleep(0.5)
+            print("Killing the rest")
+            os.system(KILL_ALL)
 
     time.sleep(3.0)
-    # os.system("sed -i '/visual/s/^#//g' $(rospack find panther)/param/panther.yaml") #comment out visual param
-    # os.system("sed -i '/mode/s/^#//g' $(rospack find panther)/param/panther.yaml") #comment out mode param
