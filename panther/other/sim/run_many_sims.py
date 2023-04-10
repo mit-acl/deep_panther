0f1de5800b8a8b041e6c0a5408f43f7e78d82544
kotakondo <kkondo@mit.edu>
2023-04-10 13:30:45 -0400

    clean up + params changed for multagent + multi-obstacles benchmarking

--- panther/other/sim/run_many_sims.py
+++ panther/other/sim/run_many_sims.py
@@ -8,7 +8,8 @@
 #  * Authors: Jesus Tordesillas, et al.
 #  * See LICENSE file for the license information
 #  *
-#  * Input: home directory
+#  * Input: directory to save bags (if not given the default is  "/media/kota/T7/deep-panther/bags")
+#  * Output: bags with the name "sim_{sim_number}_{agent_name}_{date}.bag"
 #  * -------------------------------------------------------------------------- */
 
 import math
@@ -33,21 +34,21 @@ def get_start_end_state(num_of_agents, circle_radius) -> (list, list, list, list
     y_start_list = []
     z_start_list = []
     yaw_start_list = []
-    x_end_list = []
-    y_end_list = []
-    z_end_list = []
+    x_goal_list = []
+    y_goal_list = []
+    z_goal_list = []
 
     for i in range(num_of_agents):
         angle = 2*math.pi/num_of_agents * i
         x_start_list.append(circle_radius*math.cos(angle))
         y_start_list.append(circle_radius*math.sin(angle))
         z_start_list.append(0.0)
-        yaw_start_list.append(math.tan(y_start_list[i]/x_start_list[i]) + math.pi)
-        x_end_list.append(-x_start_list[i])
-        y_end_list.append(-y_start_list[i])
-        z_end_list.append(0.0)
+        yaw_start_list.append(math.atan2(y_start_list[i], x_start_list[i]) + math.pi)
+        x_goal_list.append(-x_start_list[i])
+        y_goal_list.append(-y_start_list[i])
+        z_goal_list.append(0.0)
 
-    return x_start_list, y_start_list, z_start_list, yaw_start_list, x_end_list, y_end_list, z_end_list
+    return x_start_list, y_start_list, z_start_list, yaw_start_list, x_goal_list, y_goal_list, z_goal_list
 
 def check_goal_reached():
     try:
@@ -64,16 +65,17 @@ if __name__ == '__main__':
     ## Parameters
     ##
 
-    NUM_OF_SIMS = 100
-    NUM_OF_AGENTS = 1
-    NUM_OF_OBS_LIST = [5, 10, 15]
-    CIRCLE_RADIUS = 10.0
+    NUM_OF_SIMS = 1
+    NUM_OF_AGENTS = 2
+    NUM_OF_OBS_LIST = [1]
+    CIRCLE_RADIUS = 5.0
     USE_PERFECT_CONTROLLER = "true"
     USE_PERFECT_PREDICTION = "true"
     SIM_DURATION = 60 # in seconds
-    HOME_DIR = sys.argv[1] if len(sys.argv) > 1 else "/media/kota/T7/deep-panther/bags"
+    DATA_DIR = sys.argv[1] if len(sys.argv) > 1 else "/media/kota/T7/deep-panther/bags"
     RECORD_NODE_NAME = "bag_recorder"
     KILL_ALL = "killall -9 gazebo & killall -9 gzserver  & killall -9 gzclient & pkill -f panther & pkill -f gazebo_ros & pkill -f spawn_model & pkill -f gzserver & pkill -f gzclient  & pkill -f static_transform_publisher &  killall -9 multi_robot_node & killall -9 roscore & killall -9 rosmaster & pkill rmader_node & pkill -f tracker_predictor & pkill -f swarm_traj_planner & pkill -f dynamic_obstacles & pkill -f rosout & pkill -f behavior_selector_node & pkill -f rviz & pkill -f rqt_gui & pkill -f perfect_tracker & pkill -f rmader_commands & pkill -f dynamic_corridor & tmux kill-server & pkill -f perfect_controller & pkill -f publish_in_gazebo"
+    ROS_TOPICS = "/{}/goal /{}/state /tf /tf_static /{}/panther/fov /obstacles_mesh /{}/panther/best_solution_expert /{}/panther/best_solution_student /{}/term_goal /{}/panther/actual_traj /clock /trajs /sim_all_agents_goal_reached /{}/panther/is_ready"
 
     ##
     ## make sure ROS (and related stuff) is not running
@@ -91,9 +93,11 @@ if __name__ == '__main__':
         ## set up folders
         ##
 
-        folder_bags = HOME_DIR + f"/{NUM_OF_OBS_LIST[k]}_obs"
+        folder_bags = DATA_DIR + f"/{NUM_OF_OBS_LIST[k]}_obs"
         if not os.path.exists(folder_bags):
             os.makedirs(folder_bags)
+        date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        os.system(f'echo "\n{date}" >> '+folder_bags+'/status.txt')
         
         for s in range(NUM_OF_SIMS):
 
@@ -106,9 +110,9 @@ if __name__ == '__main__':
             time_sleep = max(0.2*NUM_OF_OBS_LIST[k], 2.0)
             time_sleep_goal = min(NUM_OF_OBS_LIST[k], NUM_OF_AGENTS, 10.0)
 
-            ###
-            ### simulation set up
-            ###
+            ##
+            ## simulation set up
+            ##
 
             ## roscore
             commands.append("roscore")
@@ -117,23 +121,25 @@ if __name__ == '__main__':
             commands.append(f"roslaunch --wait panther sim_base_station.launch num_of_obs:={NUM_OF_OBS_LIST[k]} rviz:=true gui_mission:=false")
 
             ## sim_onboard
-            x_start_list, y_start_list, z_start_list, yaw_start_list, x_end_list, y_end_list, z_end_list = get_start_end_state(NUM_OF_AGENTS, CIRCLE_RADIUS)
+            x_start_list, y_start_list, z_start_list, yaw_start_list, x_goal_list, y_goal_list, z_goal_list = get_start_end_state(NUM_OF_AGENTS, CIRCLE_RADIUS)
             for i, (x, y, z, yaw) in enumerate(zip(x_start_list, y_start_list, z_start_list, yaw_start_list)):
                 agent_name = f"SQ{str(i+1).zfill(2)}s"
                 commands.append(f"roslaunch --wait panther sim_onboard.launch quad:={agent_name} perfect_controller:={USE_PERFECT_CONTROLLER} perfect_prediction:={USE_PERFECT_PREDICTION} x:={x} y:={y} z:={z} yaw:={yaw}")
             
             ## rosbag record
-            # commands.append("sleep "+str(time_sleep)+" && cd "+folder_bags+" && rosbag record -a -o sim_"+str(s)+" __name:="+RECORD_NODE_NAME)
+            agent_bag_recorders = []
+            for i in range(NUM_OF_AGENTS):
+                agent_name = f"SQ{str(i+1).zfill(2)}s"
+                agent_bag_recorder = f"{agent_name}_{RECORD_NODE_NAME}"
+                recorded_topics = ROS_TOPICS.format(*[agent_name for i in range(8)])
+                agent_bag_recorders.append(agent_bag_recorder)
+                commands.append("sleep "+str(time_sleep)+" && cd "+folder_bags+" && rosbag record "+recorded_topics+" -o sim_"+str(s)+"_"+agent_name+" __name:="+agent_bag_recorder)
             
             ## goal checker
             commands.append(f"sleep {time_sleep} && roslaunch --wait panther goal_reached_checker.launch num_of_agents:={NUM_OF_AGENTS} circle_radius:={CIRCLE_RADIUS}")
 
             ## publish goal
-            for i, (x, y, z) in enumerate(zip(x_end_list, y_end_list, z_end_list)):
-                agent_name = f"SQ{str(i+1).zfill(2)}s"
-                ## TODO: may need to change the goal orientation
-                commands.append(f"sleep "+str(time_sleep_goal)+" && rostopic pub /"+agent_name+"/term_goal geometry_msgs/PoseStamped \'{header: {stamp: now, frame_id: \"world\"}, \
-                    pose: {position: {x: "+str(x)+", y: "+str(y)+", z: "+str(z)+"}, orientation: {w: 1.0}}}\'")
+            commands.append(f"sleep "+str(time_sleep_goal)+f" && roslaunch --wait panther pub_goal.launch x_goal_list:=\"{x_goal_list}\" y_goal_list:=\"{y_goal_list}\" z_goal_list:=\"{z_goal_list}\"")
 
             ##
             ## tmux & sending commands
@@ -174,7 +180,8 @@ if __name__ == '__main__':
                 os.system(f'echo "simulation {s}: goal reached" >> '+folder_bags+'/status.txt')
                 print("Goal is reached, killing the bag node")
 
-            os.system("rosnode kill "+RECORD_NODE_NAME)
+            for agent_bag_recorder in agent_bag_recorders:
+                os.system("rosnode kill "+agent_bag_recorder)
             time.sleep(0.5)
             print("Killing the rest")
             os.system(KILL_ALL)
