dc69bb42e9a56b8339d5a536477ee4a4675a8308
kotakondo <kkondo@mit.edu>
2023-04-21 17:59:46 -0400

    clean up run_many_sims.py

--- panther/other/sim/run_many_sims.py
+++ panther/other/sim/run_many_sims.py
@@ -66,9 +66,9 @@ if __name__ == '__main__':
     ##
 
     NUM_OF_SIMS = 100
-    NUM_OF_AGENTS = [3]
+    NUM_OF_AGENTS = [1, 3]
     NUM_OF_OBS_LIST = [2]
-    CIRCLE_RADIUS = 4.0
+    CIRCLE_RADIUS = 5.0
     USE_PERFECT_CONTROLLER = "true"
     USE_PERFECT_PREDICTION = "false"
     SIM_DURATION = 120 # in seconds
@@ -77,8 +77,8 @@ if __name__ == '__main__':
     KILL_ALL = "killall -9 gazebo & killall -9 gzserver  & killall -9 gzclient & pkill -f panther & pkill -f gazebo_ros & pkill -f spawn_model & pkill -f gzserver & pkill -f gzclient  & pkill -f static_transform_publisher &  killall -9 multi_robot_node & killall -9 roscore & killall -9 rosmaster & pkill rmader_node & pkill -f tracker_predictor & pkill -f swarm_traj_planner & pkill -f dynamic_obstacles & pkill -f rosout & pkill -f behavior_selector_node & pkill -f rviz & pkill -f rqt_gui & pkill -f perfect_tracker & pkill -f rmader_commands & pkill -f dynamic_corridor & tmux kill-server & pkill -f perfect_controller & pkill -f publish_in_gazebo"
     TOPICS_TO_RECORD = "/{}/goal /{}/state /tf /tf_static /{}/panther/fov /obstacles_mesh /{}/panther/best_solution_expert /{}/panther/best_solution_student /{}/term_goal /{}/panther/actual_traj /clock /trajs /sim_all_agents_goal_reached /{}/panther/is_ready /{}/panther/log"
     USE_RVIZ = sys.argv[2] if len(sys.argv) >2 else "true"
-    # AGENTS_TYPES = ["parm", "parm_star", "primer"]
-    AGENTS_TYPES = ["primer"]
+    AGENTS_TYPES = ["parm", "parm_star", "primer"]
+    TRAJ_NUM_PER_REPLAN_LIST = [1, 6]
 
     ##
     ## make sure ROS (and related stuff) is not running
@@ -92,135 +92,149 @@ if __name__ == '__main__':
 
     os.system("sed -i '/use_panther_star:/s/^/#/g' $(rospack find panther)/param/panther.yaml")
     os.system("sed -i '/use_expert:/s/^/#/g' $(rospack find panther)/param/panther.yaml") ## added : to avoid commenting out use_expert_for_other_agents_in_training
-    os.system("sed -i '/use_student/s/^/#/g' $(rospack find panther)/param/panther.yaml")
+    os.system("sed -i '/use_student:/s/^/#/g' $(rospack find panther)/param/panther.yaml")
+    os.system("sed -i '/num_of_trajs_per_replan:/s/^/#/g' $(rospack find panther)/param/panther.yaml")
+    os.system("sed -i '/max_num_of_initial_guesses:/s/^/#/g' $(rospack find panther)/param/panther.yaml")
 
     ##
     ## simulation loop
     ##
 
-    for agent_type in AGENTS_TYPES:
+    for traj_num in TRAJ_NUM_PER_REPLAN_LIST:
 
-        DATA_AGENT_TYPE_DIR = DATA_DIR + f"/{agent_type}"
+        for agent_type in AGENTS_TYPES:
 
-        for k in range(len(NUM_OF_OBS_LIST)):
+            if traj_num == 1 and agent_type == "primer":
+                # primer always produce 6 trajs as the NN's output is fixed
+                continue
 
-            for l in NUM_OF_AGENTS:
+            DATA_AGENT_TYPE_DIR = DATA_DIR + f"/{traj_num}_traj/{agent_type}"
 
-                ##
-                ## set up folders
-                ##
+            for k in range(len(NUM_OF_OBS_LIST)):
 
-                folder_bags = DATA_AGENT_TYPE_DIR + f"/{NUM_OF_OBS_LIST[k]}_obs_{l}_agents"
-                if not os.path.exists(folder_bags):
-                    os.makedirs(folder_bags)
-                date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-                os.system(f'echo "\n{date}" >> '+folder_bags+'/status.txt')
+                for l in NUM_OF_AGENTS:
+
+                    ##
+                    ## set up folders
+                    ##
+
+                    folder_bags = DATA_AGENT_TYPE_DIR + f"/{NUM_OF_OBS_LIST[k]}_obs_{l}_agents"
+                    if not os.path.exists(folder_bags):
+                        os.makedirs(folder_bags)
+                    date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                    os.system(f'echo "\n{date}" >> '+folder_bags+'/status.txt')
+
                 
-                for s in range(NUM_OF_SIMS):
+                    for s in range(NUM_OF_SIMS):
 
-                    ##
-                    ## commands list initialized
-                    ##
+                        ##
+                        ## commands list initialized
+                        ##
 
-                    commands = []
+                        commands = []
+                        time_sleep = max(0.2*NUM_OF_OBS_LIST[k], 2.0)
+                        time_sleep_goal = min(NUM_OF_OBS_LIST[k], l, 2.0)
 
-                    time_sleep = max(0.2*NUM_OF_OBS_LIST[k], 2.0)
-                    time_sleep_goal = min(NUM_OF_OBS_LIST[k], l, 2.0)
+                        ##
+                        ## simulation set up
+                        ##
 
-                    ##
-                    ## simulation set up
-                    ##
+                        ## roscore
+                        commands.append("roscore")
 
-                    ## roscore
-                    commands.append("roscore")
+                        ## sim_basestation
+                        commands.append(f"roslaunch --wait panther sim_base_station.launch num_of_obs:={NUM_OF_OBS_LIST[k]} rviz:={USE_RVIZ} gui_mission:=false")
+                        
+                        ## set up parameters depending on agent types
+                        for i in range(l):
+                            agent_name = f"SQ{str(i+1).zfill(2)}s"
+                            if agent_type == "parm":
+                                commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_panther_star false")
+                                commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_expert true")
+                                commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_student false")
+                                commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/num_of_trajs_per_replan {traj_num}")
+                                commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/max_num_of_initial_guesses {traj_num}")
+                            elif agent_type == "parm_star":
+                                commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_panther_star true")
+                                commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_expert true")
+                                commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_student false")
+                                commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/num_of_trajs_per_replan {traj_num}")
+                                commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/max_num_of_initial_guesses {traj_num}")
+                            elif agent_type == "primer":
+                                commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_panther_star true")
+                                commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_expert false")
+                                commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_student true")
+                                commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/num_of_trajs_per_replan 6")
+                                commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/max_num_of_initial_guesses 6")
 
-                    ## sim_basestation
-                    commands.append(f"roslaunch --wait panther sim_base_station.launch num_of_obs:={NUM_OF_OBS_LIST[k]} rviz:={USE_RVIZ} gui_mission:=false")
-                    
-                    ## set up parameters depending on agent types
-                    for i in range(l):
-                        agent_name = f"SQ{str(i+1).zfill(2)}s"
-                        if agent_type == "parm":
-                            commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_panther_star false")
-                            commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_expert true")
-                            commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_student false")
-                        elif agent_type == "parm_star":
-                            commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_panther_star true")
-                            commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_expert true")
-                            commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_student false")
-                        elif agent_type == "primer":
-                            commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_panther_star true")
-                            commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_expert false")
-                            commands.append(f"sleep 2.0 && rosparam set /{agent_name}/panther/use_student true")
+                        ## sim_onboard
+                        x_start_list, y_start_list, z_start_list, yaw_start_list, x_goal_list, y_goal_list, z_goal_list = get_start_end_state(l, CIRCLE_RADIUS)
+                        for i, (x, y, z, yaw) in enumerate(zip(x_start_list, y_start_list, z_start_list, yaw_start_list)):
+                            agent_name = f"SQ{str(i+1).zfill(2)}s"
+                            commands.append(f"sleep 5.0 && roslaunch --wait panther sim_onboard.launch quad:={agent_name} perfect_controller:={USE_PERFECT_CONTROLLER} perfect_prediction:={USE_PERFECT_PREDICTION} x:={x} y:={y} z:={z} yaw:={yaw} 2> >(grep -v -e TF_REPEATED_DATA -e buffer)")
+                        
+                        ## rosbag record
+                        agent_bag_recorders = []
+                        for i in range(l):
+                            sim_name = f"sim_{str(s).zfill(3)}"
+                            agent_name = f"SQ{str(i+1).zfill(2)}s"
+                            recorded_topics = TOPICS_TO_RECORD.format(*[agent_name for i in range(9)])
+                            agent_bag_recorder = f"{agent_name}_{RECORD_NODE_NAME}"
+                            agent_bag_recorders.append(agent_bag_recorder)
+                            commands.append("sleep "+str(time_sleep)+" && cd "+folder_bags+" && rosbag record "+recorded_topics+" -o "+sim_name+"_"+agent_name+" __name:="+agent_bag_recorder)
+                        
+                        ## goal checker
+                        commands.append(f"sleep {time_sleep} && roslaunch --wait panther goal_reached_checker.launch num_of_agents:={l} circle_radius:={CIRCLE_RADIUS}")
 
-                    ## sim_onboard
-                    x_start_list, y_start_list, z_start_list, yaw_start_list, x_goal_list, y_goal_list, z_goal_list = get_start_end_state(l, CIRCLE_RADIUS)
-                    for i, (x, y, z, yaw) in enumerate(zip(x_start_list, y_start_list, z_start_list, yaw_start_list)):
-                        agent_name = f"SQ{str(i+1).zfill(2)}s"
-                        commands.append(f"sleep 5.0 && roslaunch --wait panther sim_onboard.launch quad:={agent_name} perfect_controller:={USE_PERFECT_CONTROLLER} perfect_prediction:={USE_PERFECT_PREDICTION} x:={x} y:={y} z:={z} yaw:={yaw} 2> >(grep -v -e TF_REPEATED_DATA -e buffer)")
-                    
-                    ## rosbag record
-                    agent_bag_recorders = []
-                    for i in range(l):
-                        sim_name = f"sim_{str(s).zfill(3)}"
-                        agent_name = f"SQ{str(i+1).zfill(2)}s"
-                        recorded_topics = TOPICS_TO_RECORD.format(*[agent_name for i in range(9)])
-                        agent_bag_recorder = f"{agent_name}_{RECORD_NODE_NAME}"
-                        agent_bag_recorders.append(agent_bag_recorder)
-                        commands.append("sleep "+str(time_sleep)+" && cd "+folder_bags+" && rosbag record "+recorded_topics+" -o "+sim_name+"_"+agent_name+" __name:="+agent_bag_recorder)
-                    
-                    ## goal checker
-                    commands.append(f"sleep {time_sleep} && roslaunch --wait panther goal_reached_checker.launch num_of_agents:={l} circle_radius:={CIRCLE_RADIUS}")
+                        ## publish goal
+                        commands.append(f"sleep "+str(time_sleep_goal)+f" && roslaunch --wait panther pub_goal.launch x_goal_list:=\"{x_goal_list}\" y_goal_list:=\"{y_goal_list}\" z_goal_list:=\"{z_goal_list}\"")
 
-                    ## publish goal
-                    commands.append(f"sleep "+str(time_sleep_goal)+f" && roslaunch --wait panther pub_goal.launch x_goal_list:=\"{x_goal_list}\" y_goal_list:=\"{y_goal_list}\" z_goal_list:=\"{z_goal_list}\"")
+                        ##
+                        ## tmux & sending commands
+                        ##
 
-                    ##
-                    ## tmux & sending commands
-                    ##
+                        session_name="run_many_sims_multiagent_session"
+                        os.system("tmux kill-session -t" + session_name)
+                        os.system("tmux new -d -s "+str(session_name)+" -x 300 -y 300")
 
-                    session_name="run_many_sims_multiagent_session"
-                    os.system("tmux kill-session -t" + session_name)
-                    os.system("tmux new -d -s "+str(session_name)+" -x 300 -y 300")
+                        for i in range(len(commands)):
+                            os.system('tmux split-window ; tmux select-layout tiled')
+                        
+                        for i in range(len(commands)):
+                            os.system('tmux send-keys -t '+str(session_name)+':0.'+str(i) +' "'+ commands[i]+'" '+' C-m')
 
-                    for i in range(len(commands)):
-                        os.system('tmux split-window ; tmux select-layout tiled')
-                    
-                    for i in range(len(commands)):
-                        os.system('tmux send-keys -t '+str(session_name)+':0.'+str(i) +' "'+ commands[i]+'" '+' C-m')
+                        print("commands sent")
+                        time.sleep(3.0)
 
-                    print("commands sent")
-                    time.sleep(3.0)
+                        ##
+                        ## wait until the goal is reached
+                        ##
 
-                    ##
-                    ## wait until the goal is reached
-                    ##
-
-                    is_goal_reached = False
-                    tic = time.perf_counter()
-                    toc = time.perf_counter()
-
-                    while (toc - tic < SIM_DURATION and not is_goal_reached):
+                        is_goal_reached = False
+                        tic = time.perf_counter()
                         toc = time.perf_counter()
-                        if(check_goal_reached()):
-                            print('all the agents reached the goal')
-                            is_goal_reached = True
-                        time.sleep(0.1)
 
-                    if (not is_goal_reached):
-                        os.system(f'echo "simulation {s}: not goal reached" >> '+folder_bags+'/status.txt')
-                        print("Goal is not reached, killing the bag node")
-                    else:
-                        os.system(f'echo "simulation {s}: goal reached" >> '+folder_bags+'/status.txt')
-                        print("Goal is reached, killing the bag node")
+                        while (toc - tic < SIM_DURATION and not is_goal_reached):
+                            toc = time.perf_counter()
+                            if(check_goal_reached()):
+                                print('all the agents reached the goal')
+                                is_goal_reached = True
+                            time.sleep(0.1)
 
-                    for agent_bag_recorder in agent_bag_recorders:
-                        os.system("rosnode kill "+agent_bag_recorder)
-                    time.sleep(0.5)
-                    print("Killing the rest")
-                    os.system(KILL_ALL)
+                        if (not is_goal_reached):
+                            os.system(f'echo "simulation {s}: not goal reached" >> '+folder_bags+'/status.txt')
+                            print("Goal is not reached, killing the bag node")
+                        else:
+                            os.system(f'echo "simulation {s}: goal reached" >> '+folder_bags+'/status.txt')
+                            print("Goal is reached, killing the bag node")
 
-            time.sleep(3.0)
+                        for agent_bag_recorder in agent_bag_recorders:
+                            os.system("rosnode kill "+agent_bag_recorder)
+                        time.sleep(0.5)
+                        print("Killing the rest")
+                        os.system(KILL_ALL)
+
+                        time.sleep(3.0)
 
     ## 
     ## uncomment delay_check param
@@ -228,4 +242,6 @@ if __name__ == '__main__':
 
     os.system("sed -i '/use_panther_star:/s/^#//g' $(rospack find panther)/param/panther.yaml")
     os.system("sed -i '/use_expert:/s/^#//g' $(rospack find panther)/param/panther.yaml") ## added : to avoid commenting out use_expert_for_other_agents_in_training
-    os.system("sed -i '/use_student/s/^#//g' $(rospack find panther)/param/panther.yaml")
+    os.system("sed -i '/use_student:/s/^#//g' $(rospack find panther)/param/panther.yaml")
+    os.system("sed -i '/num_of_trajs_per_replan:/s/^#//g' $(rospack find panther)/param/panther.yaml")
+    os.system("sed -i '/max_num_of_initial_guesses:/s/^#//g' $(rospack find panther)/param/panther.yaml")
